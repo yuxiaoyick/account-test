7d1806a1a1bdb6765f30474aea129d1e122e0d33
yuxiaoyick <admin@cuideMacBookPro.local>
2017-07-04 14:56:42 +0800

    还原

--- src/App.vue
+++ src/App.vue
@@ -24,8 +24,9 @@
         fullStyle: [
           `/*
 * Inspired by http://strml.net/
-* 大家好，我是崔坤
-* 这是我的简历！
+* 大家好，我是方方 
+* 二月了，好多公司都在招聘，你是不是也在准备简历呀。
+* 说做就做，我也来写一份简历！
 */
 
 /* 首先给所有元素加上过渡效果 */
@@ -35,7 +36,7 @@
 }
 /* 白色背景太单调了，我们来点背景 */
 html {
-  color: rgb(222,222,222); background: rgb(0,43,54);
+  color: rgb(222,222,222); background: rgb(0,43,54); 
 }
 /* 文字离边框太近了 */
 .styleEditor {
@@ -57,8 +58,8 @@ html{
           perspective: 1000px;
 }
 .styleEditor {
-  position: fixed; left: 0; top: 0;
-  -webkit-transition: none;
+  position: fixed; left: 0; top: 0; 
+  -webkit-transition: none; 
   transition: none;
   -webkit-transform: rotateY(10deg) translateZ(-100px) ;
           transform: rotateY(10deg) translateZ(-100px) ;
@@ -68,7 +69,7 @@ html{
 .resumeEditor{
   position: fixed; right: 0; top: 0;
   padding: .5em;  margin: .5em;
-  width: 48vw; height: 90vh;
+  width: 48vw; height: 90vh; 
   border: 1px solid;
   background: white; color: #222;
   overflow: auto;
@@ -105,8 +106,8 @@ html{
   counter-reset: section;
 }
 .resumeEditor ol li::before {
-  counter-increment: section;
-  content: counters(section, ".") " ";
+  counter-increment: section;            
+  content: counters(section, ".") " ";  
   margin-right: .5em;
 }
 .resumeEditor blockquote {
@@ -116,15 +117,14 @@ html{
 }
 `],
         currentMarkdown: '',
-        fullMarkdown: `崔坤
+        fullMarkdown: `方应杭
 ----
 
-资深iOS工程师，资深iOS讲师。
+资深前端工程师，资深前端讲师，现在在 [饥人谷](http://jirengu.com) 教前端课程。
 
 技能
 ----
 
-* iOS开发
 * 前端开发
 * Rails 开发
 * Node.js 开发
@@ -225,11 +225,11 @@ html{
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
   }
-
+  
   html {
     min-height: 100vh;
   }
-
+  
   * {
     -webkit-transition: all .3s;
     transition: all .3s;
