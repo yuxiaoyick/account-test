ebc2b89533f58caf70358cef03170fff0f3f1b91
yuxiaoyick <admin@cuideMacBookPro.local>
2017-07-04 15:51:45 +0800

    修改 并 npm run build

--- dist/static/js/app.816230e599f3c20cff07.js
+++ dist/static/js/app.816230e599f3c20cff07.js
@@ -60,27 +60,26 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
       enableHtml: false,
       fullStyle: [`/*
 * Inspired by http://strml.net/
-* 大家好，我是方方 
-* 二月了，好多公司都在招聘，你是不是也在准备简历呀。
-* 说做就做，我也来写一份简历！
+* 哎，终于弄好了！！
+* 可以装biu了，哈哈哈哈哈哈哈哈哈。。。
 */
 
 /* 首先给所有元素加上过渡效果 */
 * {
-  -webkit-transition: all .3s;
-  transition: all .3s;
+-webkit-transition: all .3s;
+transition: all .3s;
 }
 /* 白色背景太单调了，我们来点背景 */
 html {
-  color: rgb(222,222,222); background: rgb(0,43,54); 
+color: rgb(222,222,222); background: rgb(0,43,54);
 }
 /* 文字离边框太近了 */
 .styleEditor {
-  padding: .5em;
-  border: 1px solid;
-  margin: .5em;
-  overflow: auto;
-  width: 45vw; height: 90vh;
+padding: .5em;
+border: 1px solid;
+margin: .5em;
+overflow: auto;
+width: 45vw; height: 90vh;
 }
 /* 代码高亮 */
 .token.selector{ color: rgb(133,153,0); }
@@ -90,94 +89,91 @@ html {
 
 /* 加点 3D 效果呗 */
 html{
-  -webkit-perspective: 1000px;
-          perspective: 1000px;
+-webkit-perspective: 1000px;
+perspective: 1000px;
 }
 .styleEditor {
-  position: fixed; left: 0; top: 0; 
-  -webkit-transition: none; 
-  transition: none;
-  -webkit-transform: rotateY(10deg) translateZ(-100px) ;
-          transform: rotateY(10deg) translateZ(-100px) ;
+position: fixed; left: 0; top: 0;
+-webkit-transition: none;
+transition: none;
+-webkit-transform: rotateY(10deg) translateZ(-100px) ;
+transform: rotateY(10deg) translateZ(-100px) ;
 }
 
 /* 接下来我给自己准备一个编辑器 */
 .resumeEditor{
-  position: fixed; right: 0; top: 0;
-  padding: .5em;  margin: .5em;
-  width: 48vw; height: 90vh; 
-  border: 1px solid;
-  background: white; color: #222;
-  overflow: auto;
+position: fixed; right: 0; top: 0;
+padding: .5em;  margin: .5em;
+width: 48vw; height: 90vh;
+border: 1px solid;
+background: white; color: #222;
+overflow: auto;
 }
 /* 好了，我开始写简历了 */
 
 
 `, `
 /* 这个简历好像差点什么
- * 对了，这是 Markdown 格式的，我需要变成对 HR 更友好的格式
- * 简单，用开源工具翻译成 HTML 就行了
- */
+* 对了，这是 Markdown 格式的，我需要变成对 HR 更友好的格式
+* 简单，用开源工具翻译成 HTML 就行了
+*/
 `, `
 /* 再对 HTML 加点样式 */
 .resumeEditor{
-  padding: 2em;
+padding: 2em;
 }
 .resumeEditor h2{
-  display: inline-block;
-  border-bottom: 1px solid;
-  margin: 1em 0 .5em;
+display: inline-block;
+border-bottom: 1px solid;
+margin: 1em 0 .5em;
 }
 .resumeEditor ul,.resumeEditor ol{
-  list-style: none;
+list-style: none;
 }
 .resumeEditor ul> li::before{
-  content: '•';
-  margin-right: .5em;
+content: '•';
+margin-right: .5em;
 }
 .resumeEditor ol {
-  counter-reset: section;
+counter-reset: section;
 }
 .resumeEditor ol li::before {
-  counter-increment: section;            
-  content: counters(section, ".") " ";  
-  margin-right: .5em;
+counter-increment: section;
+content: counters(section, ".") " ";
+margin-right: .5em;
 }
 .resumeEditor blockquote {
-  margin: 1em;
-  padding: .5em;
-  background: #ddd;
+margin: 1em;
+padding: .5em;
+background: #ddd;
 }
 `],
       currentMarkdown: '',
-      fullMarkdown: `方应杭
+      fullMarkdown: `看什么看
 ----
 
-资深前端工程师，资深前端讲师，现在在 [饥人谷](http://jirengu.com) 教前端课程。
+说的就是你！大胡子
 
-技能
+（大胡子的）技能
 ----
 
-* 前端开发
-* Rails 开发
-* Node.js 开发
-* 前端授课
+* 胡子大
+* biu界大佬
+* 勾搭妹子
+* 勾搭汉子
 
 工作经历
 ----
 
-1. [饥人谷](http://jirengu.com)
+1. 家边网络科技有限公司
 2. 腾讯即时通讯平台部
 3. 阿里巴巴B2B部门
-4. 彩程知人项目组
 
 链接
 ----
 
-* [GitHub](https://github.com/frankfang)
-* [我的文章](https://www.zhihu.com/people/zhihusucks/pins/posts)
-
-> 如果你喜欢这个效果，Fork [我的项目](https://github.com/jirengu-inc/animating-resume)，打造你自己的简历！
+* [GitHub](https://github.com/yuxiaoyick/account-test)
+* [我的文章](https://github.com/yuxiaoyick/account-test)
 
 `
     };
@@ -470,4 +466,4 @@ module.exports={render:function (){var _vm=this;var _h=_vm.$createElement;var _c
 
 /***/ })
 ],[7]);
-//# sourceMappingURL=app.bcebe3e9a1f062c2df51.js.map
+//# sourceMappingURL=app.816230e599f3c20cff07.js.map
